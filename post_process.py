3104d5f6990761a1b0fb8084209f58f21789bb73
Dmitry Bandurin <dmitry.bandurin@Dmitrys-MacBook-Pro-2.local>
2019-10-05 12:41:01 -0500

    some comments added

--- post_process.py
+++ post_process.py
@@ -1,2 +1,4 @@
+#Text preprocessing
+#spell-checking, grammar correction, removing non-textual elements
 def text_process(text):
     return text
--- pre_process.py
+++ pre_process.py
@@ -1,2 +1,4 @@
+# Prepare the input image in order to do better text recognition in OCR component 
+# (e.g. removal of borders, background cleaning, anti-blurring filters, text segmentation).
 def image_process(img):
     return img
